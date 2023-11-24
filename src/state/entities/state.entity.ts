98e78fdb9d3748a8ef993df60674afd84bbe2602
Augusto Neves <augustoneves@frwk.com.br>
2023-11-24 11:34:59 -0300

    feat: adding state route to get all states

---
+++ src/state/entities/state.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'state' })
+export class StateEntity {
+  @PrimaryGeneratedColumn('rowid')
+  id: number;
+
+  @Column({ name: 'name', nullable: false })
+  name: string;
+
+  @Column({ name: 'uf', nullable: false })
+  uf: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  created_at: Date;
+
+  @CreateDateColumn({ name: 'updated_at' })
+  updated_at: Date;
+}
