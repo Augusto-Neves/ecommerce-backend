a6a6dd073afc819c2f50b05dab6e3e903ade9d2c
Augusto Neves <augustoneves@frwk.com.br>
2023-11-27 17:20:20 -0300

    refactor: change route name to states

--- src/state/state.controller.ts
+++ src/state/state.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { StateService } from './state.service';
 import { StateEntity } from './entities/state.entity';
 
-@Controller('state')
+@Controller('states')
 export class StateController {
   constructor(private readonly stateService: StateService) {}
 
