fe7da0fd7133d41b0f66047ceb14c58bd66ed085
Augusto Neves <augustoneves@frwk.com.br>
2023-12-13 19:58:51 -0300

    feat: adding auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { AuthService } from './auth.service';
+import { ReturnLoginDto } from './dto/returnLogin.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UsePipes(ValidationPipe)
+  @Post()
+  async login(@Body() loginDto: LoginDto): Promise<ReturnLoginDto> {
+    return this.authService.login(loginDto);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { LoginDto } from './dto/login.dto';
+import { UserService } from 'src/user/user.service';
+import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ReturnLoginDto } from './dto/returnLogin.dto';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { LoginPayloadDto } from './dto/loginPayload.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(loginDto: LoginDto): Promise<ReturnLoginDto> {
+    const user: UserEntity | undefined = await this.userService
+      .findUserByEmail(loginDto.email)
+      .catch(() => undefined);
+
+    let userPassword = '';
+
+    if (user) {
+      userPassword = user.password;
+    }
+
+    const matchPassword = await compare(loginDto.password, userPassword);
+
+    if (!user || !matchPassword) {
+      throw new NotFoundException('Email or password invalid');
+    }
+
+    return {
+      access_token: this.jwtService.sign({ ...new LoginPayloadDto(user) }),
+      user: new ReturnUserDto(user),
+    };
+  }
+}
