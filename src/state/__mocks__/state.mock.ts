38a5d1eaf1160cc5177b08e6c3918af68e650b0e
Augusto Neves <augustoneves@frwk.com.br>
2023-12-18 23:12:39 -0300

    feat: creating test to state module

---
+++ src/state/__mocks__/state.mock.ts
@@ -0,0 +1,9 @@
+import { StateEntity } from '../entities/state.entity';
+
+export const stateMock: StateEntity = {
+  created_at: new Date(),
+  id: 12,
+  name: 'State Name Mock',
+  uf: 'STM',
+  updated_at: new Date(),
+};
---
+++ src/state/__tests__/state.controller.spec.ts
@@ -0,0 +1,50 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CacheService } from '../../cache/cache.service';
+import { StateController } from '../state.controller';
+import { StateService } from '../state.service';
+import { stateMock } from '../__mocks__/state.mock';
+import { StateEntity } from '../entities/state.entity';
+
+describe('StateController', () => {
+  let controller: StateController;
+  let service: StateService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [StateController],
+      providers: [
+        StateService,
+        {
+          provide: CacheService,
+          useValue: {
+            getCacheData: jest.fn().mockResolvedValue([stateMock]),
+          },
+        },
+        {
+          provide: getRepositoryToken(StateEntity),
+          useValue: {
+            find: jest.fn().mockResolvedValue([stateMock]),
+            findOne: jest.fn().mockResolvedValue(stateMock),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<StateController>(StateController);
+    service = module.get<StateService>(StateService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  it('should return all states', async () => {
+    jest.spyOn(service, 'getAllStates').mockResolvedValue([stateMock]);
+
+    const states = await controller.getAllStates();
+
+    expect(states).toEqual([stateMock]);
+  });
+});
