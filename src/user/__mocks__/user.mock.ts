c34495d72ee7776b7c0f4c15663113174097dd74
Augusto Neves <augustoneves@frwk.com.br>
2023-12-18 23:14:27 -0300

    test: adjusting some tests

--- src/user/__mocks__/user.mock.ts
+++ src/user/__mocks__/user.mock.ts
@@ -7,7 +7,7 @@ export const userEntityMock: UserEntity = {
   email: 'user@example.com',
   id: 1987,
   name: 'John',
-  password: 'wqeyguia1689qqhbgt1u768687h',
+  password: '$2b$12$qiraArz3R2w6DWuymtfOI.hC03ZEXPfin5TkrcVu3s5U/9U9iiehm',
   phone: '87988754654',
   type_user: UserType.User,
   updated_at: new Date(),
