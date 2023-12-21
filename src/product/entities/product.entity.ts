6b7912b4413eb6b58de0397a1d043519864680a4
Augusto Neves <augustoneves@frwk.com.br>
2023-12-20 21:56:19 -0300

    feat: creating product module

---
+++ src/product/entities/product.entity.ts
@@ -0,0 +1,40 @@
+import { CategoryEntity } from '../../category/entities/category.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'product' })
+export class ProductEntity {
+  @PrimaryGeneratedColumn('rowid')
+  id: number;
+
+  @Column({ name: 'category_id', nullable: false })
+  category_id: number;
+
+  @Column({ name: 'price', nullable: false })
+  price: number;
+
+  @Column({ name: 'name', nullable: false })
+  name: string;
+
+  @Column({ name: 'image', nullable: false })
+  image: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  created_at: Date;
+
+  @CreateDateColumn({ name: 'updated_at' })
+  updated_at: Date;
+
+  @ManyToOne(
+    () => CategoryEntity,
+    (category: CategoryEntity) => category.products,
+  )
+  @JoinColumn({ name: 'category_id', referencedColumnName: 'id' })
+  category: CategoryEntity;
+}
