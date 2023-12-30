e7349257a07bb5f6e157b963d609340e633030fe
Augusto Neves <augustoneves@frwk.com.br>
2023-12-29 21:31:15 -0300

    feat: creating cart entity

---
+++ src/cart/entities/cart.entity.ts
@@ -0,0 +1,29 @@
+import { CartProductEntity } from '../../cart-product/entities/cart-product.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'cart' })
+export class CartEntity {
+  @PrimaryGeneratedColumn('rowid')
+  id: number;
+
+  @Column({ name: 'user_id', nullable: false })
+  user_id: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  created_at: Date;
+
+  @CreateDateColumn({ name: 'updated_at' })
+  updated_at: Date;
+
+  @OneToMany(
+    () => CartProductEntity,
+    (cart_product: CartProductEntity) => cart_product.cart,
+  )
+  cart_product?: CartProductEntity[];
+}
