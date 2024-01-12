8b0ae5a30bad6f0614a735ef225922f364338f24
Augusto Neves <augustoneves@frwk.com.br>
2024-01-12 14:57:57 -0300

    feat: creating Order Product module

---
+++ src/order-product/entities/order-product.entity.ts
@@ -0,0 +1,42 @@
+import { OrderEntity } from '../../order/entities/order.entity';
+import { ProductEntity } from '../../product/entities/product.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'payment_status' })
+export class OrderProductEntity {
+  @PrimaryGeneratedColumn('rowid')
+  id: number;
+
+  @Column({ name: 'order_id', nullable: false })
+  order_id: number;
+
+  @Column({ name: 'product_id', nullable: false })
+  product_id: number;
+
+  @Column({ name: 'amount', nullable: false })
+  amount: number;
+
+  @Column({ name: 'price', nullable: false })
+  price: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  created_at: Date;
+
+  @CreateDateColumn({ name: 'updated_at' })
+  updated_at: Date;
+
+  @ManyToOne(() => OrderEntity, (order) => order.orders_product)
+  @JoinColumn({ name: 'order_id', referencedColumnName: 'id' })
+  order?: OrderEntity;
+
+  @ManyToOne(() => ProductEntity, (product) => product.orders_product)
+  @JoinColumn({ name: 'product_id', referencedColumnName: 'id' })
+  product?: ProductEntity;
+}
