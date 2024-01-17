bba442bbc9a934f747713cca5eca8a07daf8c460
Augusto Neves <augustoneves@frwk.com.br>
2024-01-16 23:04:40 -0300

    refactor: removing wrong columns from payment_status

--- src/payment-status/entities/payment-status.entity.ts
+++ src/payment-status/entities/payment-status.entity.ts
@@ -1,12 +1,8 @@
 import { PaymentEntity } from '../../payment/entities/payment.entity';
-import { CartEntity } from '../../cart/entities/cart.entity';
-import { ProductEntity } from '../../product/entities/product.entity';
 import {
   Column,
   CreateDateColumn,
   Entity,
-  JoinColumn,
-  ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
@@ -16,31 +12,14 @@ export class PaymentStatusEntity {
   @PrimaryGeneratedColumn('rowid')
   id: number;
 
-  @Column({ name: 'cart_id', nullable: false })
-  cart_id: number;
-
-  @Column({ name: 'product_id', nullable: false })
-  product_id: number;
-
-  @Column({ name: 'amount', nullable: false })
-  amount: number;
+  @Column({ name: 'name', nullable: false })
+  name: number;
 
   @CreateDateColumn({ name: 'created_at' })
   created_at: Date;
 
   @CreateDateColumn({ name: 'updated_at' })
   updated_at: Date;
-
-  @ManyToOne(
-    () => ProductEntity,
-    (product: ProductEntity) => product.cart_product,
-  )
-  @JoinColumn({ name: 'product_id', referencedColumnName: 'id' })
-  product?: ProductEntity;
-
-  @ManyToOne(() => CartEntity, (cart: CartEntity) => cart.cart_product)
-  @JoinColumn({ name: 'cart_id', referencedColumnName: 'id' })
-  cart?: CartEntity;
 
   @OneToMany(() => PaymentEntity, (payment) => payment.payment_status)
   payments?: PaymentEntity[];
