89398be9390ab1bb2b5d55f300dee37539db49b6
Augusto Neves <augustoneves@frwk.com.br>
2024-01-24 12:28:36 -0300

    feat: adding amount of products on ReturnCategoryDto

--- src/category/dtos/returnCategory.dto.ts
+++ src/category/dtos/returnCategory.dto.ts
@@ -3,9 +3,11 @@ import { CategoryEntity } from '../entities/category.entity';
 export class ReturnCategoryDto {
   id: number;
   name: string;
+  amount_products?: number;
 
-  constructor(categoryEntity: CategoryEntity) {
+  constructor(categoryEntity: CategoryEntity, amount_products?: number) {
     this.id = categoryEntity.id;
     this.name = categoryEntity.name;
+    this.amount_products = Number(amount_products);
   }
 }
