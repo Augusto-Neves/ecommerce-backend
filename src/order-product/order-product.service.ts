8cabe566eda4725ddb4fcfe9f2d0c3c92300f2d0
Augusto Neves <augustoneves@frwk.com.br>
2024-01-29 22:11:48 -0300

    feat: adding findAmountOfProductsByOrderId function on order-product service

--- src/order-product/order-product.service.ts
+++ src/order-product/order-product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { OrderProductEntity } from './entities/order-product.entity';
+import { ReturnGroupOrderDto } from './dtos/returnGroupOrder.dto';
 
 @Injectable()
 export class OrderProductService {
@@ -26,5 +27,16 @@ export class OrderProductService {
     await this.orderProductRepository.insert(newOrderProduct);
 
     return newOrderProduct;
+  }
+
+  async findAmountOfProductsByOrderId(
+    order_id: number[],
+  ): Promise<ReturnGroupOrderDto[]> {
+    return this.orderProductRepository
+      .createQueryBuilder('order_product')
+      .select('order_product.order_id, COUNT(*) as total')
+      .where('order_product.order_id IN (:...ids)', { ids: order_id })
+      .groupBy('order_product.order_id')
+      .getRawMany();
   }
 }
